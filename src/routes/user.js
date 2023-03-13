ae171a581ad1355e559031351c860994107b2826
artashesblbulyan <artashesblbulyan@gmail.com>
2023-03-14 02:31:35 +0400

    Add records to the database

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,8 @@
-import {Button, Container, Typography} from "@mui/material";
+import { CheckBox } from "@mui/icons-material";
+import {Button, Checkbox, Container, TextField, Typography} from "@mui/material";
 import { getAuth } from "firebase/auth";
-import { addDoc, collection, getDocs } from "firebase/firestore";
-import {useEffect, useState} from "react";
+import { addDoc, collection, doc, getDocs, setDoc } from "firebase/firestore";
+import {useCallback, useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import { app, database, db, signInUser } from "../firebase";
 import { endSession, getSession, isLoggedIn } from "../storage/session";
@@ -9,34 +10,41 @@ import { endSession, getSession, isLoggedIn } from "../storage/session";
 export default function User() {
 
   let navigate = useNavigate()
-  const [post,setPost] = useState("")
+  const [posts,setPosts] = useState("")
   const [email, setEmail] = useState("");
-  const postList = collection(db,'posts')
+  const [userId,setUserId] = useState("sss")
+  const [title,setTitle] = useState("")
+  const [text,setText] = useState("")
+  const [image,setImage] = useState("")
+  const [date,setDate] = useState("date")
+  const [share,setShare] = useState(false)
 
-console.log(postList)
-  useEffect(()=>{
-    const getPost = async()=>{
+const getPost = useCallback(async ()=>{
       try{
-        const data = await getDocs(postList)
-        setPost(data.docs.map((doc)=>({...doc.data(),id:doc.id})))
-        console.log(data.docs.map((doc)=>({...doc.data(),id:doc.id})))
+        let loginResponse = await getAuth(app);
+        await addDoc(collection(db, "Post"), {
+          userId:loginResponse.lastNotifiedUid,
+          title,
+          text,
+          image,
+          date,
+          share,
+        });
+
+        // const data = await getDocs(postList)
+        // setPosts(data.docs.map((doc)=>({...doc.data(),id:doc.id})))
         
       }catch(err){
-        console.log("err")
+        console.log(err)
       }
-      let loginResponse = await getAuth(app);
-      console.log(loginResponse,"DDDDDDDDDDDDDDD");
-      const postPost = await addDoc(db,{
-        id:loginResponse.lastNotifiedUid,
-        title:"Hello",
-        value:"post"
-      })
+      
+      },[title,
+        text,
+        image,
+        date,share])
 
-    }
-    getPost()
-  },[])
   useEffect(() => {
-  
+    
     if (!isLoggedIn()) {
       navigate("/login");
     }
@@ -52,18 +60,46 @@ console.log(postList)
     navigate("/login");
   }
 
+  const addPost =()=>{
+    getPost()
+    // console.log()
+  }
+
+
+
   return (
     <Container maxWidth="xs" sx={{mt: 2}}>
-      <Typography variant="h6" component="h1" textAlign="center" gutterBottom>
-        You're logged in as:
-        {}
-      </Typography>
-      <Typography variant="h5" component="h1" textAlign="center" gutterBottom>
-        {email}
-      </Typography>
-      <Typography variant="p" component="p" textAlign="center" gutterBottom>
-        Check the console for your (access/session) token.
-      </Typography>
+       <TextField
+          label="Title"
+          variant="outlined"
+          type="text"
+          autoComplete="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          sx={{mt: 1}}
+          fullWidth
+        />
+        <TextField
+          label="Text"
+          variant="outlined"
+          type="text"
+          autoComplete="Text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          sx={{mt: 3}}
+          fullWidth
+        />
+       <Button variant="contained" component="label">
+      Upload Image
+    <input hidden  accept="image/*" onChange={(e) => {setImage(URL.createObjectURL(e.target.files[0]))
+      }} multiple type="file" />
+    </Button>
+        <Button variant="contained" onClick={addPost} type="submit" sx={{mt: 3}} fullWidth>add Post</Button>
+        <Checkbox
+          checked={share}
+          onChange={()=>setShare(!share)}
+          inputProps={{ 'aria-label': 'controlled' }}
+/>
       <Button variant="contained" color="error" onClick={onLogout} sx={{mt: 3}} fullWidth>
         Log out
       </Button>

a77e2f949af4fa4f96c8f36f9953eb8b9dccbfaf
Vahe <rubinyan559@gmail.com>
2023-04-09 23:00:17 +0400

    Contact design add and +1 package

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,42 +3,51 @@ import { CgArrowLongDown } from "react-icons/cg";
 import { Link } from "react-router-dom";
 import { REGISTER_PAGE } from "../../RoutePath/RoutePath";
 import { isLoggedIn } from "../../storage/session";
+import { useContactStyles } from "./Contactc.style";
+import contact from "../../imgs/contact.jpg";
+import phoneLogo from "../../imgs/phone_logo.png";
 
 export default function Main() {
-    //styles
-    const styles = useMainStyles();
+  //styles
+  const styles = useContactStyles();
 
-    return (
-        <div className={styles.intro}>
-            <div className={styles.container}>
-                <div className={styles.introInner}>
-                    <div className={styles.introTitle}>
-                        Contacts
-                    </div>
-                    <p className={styles.introText}>
-                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered
-                        alteration in some form, by injected humour, or randomised words which don't look even slightly
-                        believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't
-                        anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the
-                        Internet tend to repeat predefined chunks as necessary, making this the first true generator on
-                        the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model
-                        sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum
-                        is therefore always free from repetition, injected humour, or non-characteristic words etc.
-                        <span>
-              <CgArrowLongDown className={styles.arrowIcon} />
-            </span>
-                    </p>
-                    {!isLoggedIn() ? (
-                        <Link to={REGISTER_PAGE} className={styles.registerButton}>
-                            Register
-                        </Link>
-                    ) : (
-                        <Link to="/" className={styles.learnButton}>
-                            Go to Home
-                        </Link>
-                    )}
-                </div>
+  return (
+    <div className={styles.contact}>
+      <div className={styles.container}>
+        <div className={styles.contactInner}>
+          <div className={styles.leftBlock}>
+            <img src={contact} className={styles.contactImg} alt=""/>
+            <div className={styles.info}>
+              <p className={styles.infoText}>
+                Comfort produce husband boy her had hearing. Law others theirs
+                passed but wishes. You day real less till dear read. Considered
+                use dispatched melancholy sympathize discretion led.
+              </p>
             </div>
+          </div>
+          <div className={styles.rightBlock}>
+            <h2 className={styles.contactTitle}>Contact Us</h2>
+            <div className={styles.contactText}>
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum. ​Ut
+              enim ad minim veniam aute irure dolor in reprehenderit in
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </div>
+            <div className={styles.phone}>
+              <div className={styles.phoneLogo}>
+                <img src={phoneLogo} alt="" style={{width:"43px", height:"43px"}}/>
+              </div>
+              <div className={styles.phoneContent}>
+                <p className={styles.contentTitle}>Call Us Anytime</p>
+                <p className={styles.phoneNumbers}>+374 99-59-05-59</p>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/components/Contact/Contactc.style.js
+++ src/components/Contact/Contactc.style.js
@@ -1,7 +1,86 @@
-import  {createUseStyles} from 'react-jss'
+import { createUseStyles } from "react-jss";
+import "@fontsource/montserrat";
 
-export const useStyles  = createUseStyles({
-    contactDivMain:{
-        paddingTop:'50px'
-    }
-})
+export const useContactStyles = createUseStyles({
+  contact: {
+    width: "100vw",
+    height: "100vh",
+  },
+  container: {
+    maxWidth: "1000px",
+    margin: "0 auto",
+  },
+  contactInner: {
+    display: "flex",
+    "@media (max-width: 955px)": {
+      flexDirection:"column",
+      alignItems:"center",
+      gap:"120px",
+    },
+  },
+  leftBlock: {
+    position: "relative",
+  },
+  rightBlock: {
+    paddingTop: "100px",
+  },
+  contactImg: {
+    width: "470px",
+    height: "410px",
+  },
+  info: {
+    width: "340px",
+    height: "250px",
+    backgroundColor: "#01bdda",
+    position: "absolute",
+    top: "360px",
+    left: "-15px",
+    padding: "10px 30px",
+  },
+  infoText: {
+    marginTop: "20px",
+    fontSize: "1rem",
+    lineHeight: "2",
+    color: "white",
+  },
+  contactTitle: {
+    fontSize: "4em",
+    lineHeight: "1.1",
+    fontWeight: "600",
+    textTransform: "uppercase",
+    color: "#01bdda",
+    marginBottom: "20px",
+  },
+  contactText: {
+    fontSize: "1rem",
+    width: "365px",
+    marginBottom: "50px",
+  },
+  phone: {
+    display: "flex",
+    alignItems: "center",
+  },
+  phoneLogo: {
+    width: "70px",
+    height: "70px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#01bdda",
+    borderRadius: "50%",
+    textAlign: "center",
+  },
+  phoneContent: {
+    marginLeft: "20px",
+  },
+  contentTitle: {
+    fontWeight: "400",
+    fontSize: "1.8rem",
+    fontFamily: "Montserrat",
+  },
+  phoneNumbers: {
+    fontSize: "1.5rem",
+    fontWeight: "300",
+    wordSpacing: "1",
+  },
+});
